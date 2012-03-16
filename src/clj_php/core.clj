c0a4c4c933e76f3a67abae09dfdf529854e3f6b4
Tamreen Khan <historium@gmail.com>
2012-03-16 03:07:19 -0400

    Compile if as an expression or statement depending on the context.

--- src/clj_php/core.clj
+++ src/clj_php/core.clj
@@ -10,14 +10,16 @@
   #{:if})
 
 (defmulti compile-special-form
-  (fn [expr] (keyword (first expr))))
+  (fn [expr] (keyword [(first expr) (:context (meta expr))])))
 
-(defmethod compile-special-form :if [expr]
+(defmethod compile-special-form [:if :stmt] [expr]
   (str "if(" (compile-expr (second expr)) "){\n"
        (compile-stmt (nth expr 2))
        "}else{\n"
        (compile-stmt (nth expr 3))
        "}"))
+(defmethod compile-special-form [:if :expr] [[test if-expr else-expr]]
+  (str (compile-expr if-expr) " ? " (compile-expr if-expr) " : " (compile-expr else-expr)))
 
 (defn special-form? [expr]
   (special-forms (keyword (first expr))))
@@ -33,16 +35,16 @@
   (str "array(" (join ", " (map compile-expr expr)) ")"))
 
 (defn compile-stmt [expr]
-  (str (compile-expr expr) ";\n"))
+  (str (compile-expr (with-meta expr {:context :stmt})) ";\n"))
 
 (defn compile-expr [expr]
-  (let [compile-func
-        (cond
-         (atom? expr) compile-php-atom
-         (special-form? expr) compile-special-form
-         (list? expr) compile-func-call
-         (vector? expr) compile-array)]
+  (let [expr (if (not (:context expr))
+               (with-meta expr {:context :expr})
+               expr)
+        compile-func (cond
+                      (atom? expr) compile-php-atom
+                      (special-form? expr) compile-special-form
+                      (list? expr) compile-func-call
+                      (vector? expr) compile-array)]
     (compile-func expr)))
 
-(defn test [expr]
-  (print (compile-expr expr)))
